73df73f753d5bfe91c48c7329a9be8f81558cdd2
Erfan <erfanmelon@yahoo.com>
2023-02-02 16:18:34 +0330

    Update ResultDto.cs
    add some constructors for clear some codes

--- Store.Common/Dto/ResultDto.cs
+++ Store.Common/Dto/ResultDto.cs
@@ -1,16 +1,40 @@
-﻿namespace Store.Common.Dto
+﻿namespace Store.Common.Dto;
+
+public class ResultDto
 {
-    public class ResultDto
+    public bool IsSuccess { get; set; } = false;
+    public string Message { get; set; }
+    public ResultDto()
     {
-        public bool IsSuccess { get; set; } = false;
-        public string Message { get; set; }
+
     }
-    public class ResultDto<TKey>:ResultDto
+    public ResultDto(string message = "خطا")
     {
-        public TKey Data { get; set; } 
+        Message = message;
     }
-    public class ResultDtoError:ResultDto // try-catch usage (for catch) 
+    public ResultDto(bool isSuccess,string message="")
     {
-        public string Message { get; set; } = "خطایی رخ داد !";
+        IsSuccess = isSuccess;
+        Message = message;
     }
+}
+public class ResultDto<TKey>:ResultDto
+{
+    public TKey Data { get; set; }
+    public ResultDto()
+    {
+
+    }
+    public ResultDto(TKey data,bool isSuccess=true, string message = ""):base(isSuccess,message)
+    {
+        Data = data;
+    }
+    public ResultDto(string message = "خطا"):base(message)
+    {
+
+    }
+}
+public class ResultDtoError:ResultDto // try-catch usage (for catch) 
+{
+    public string Message { get; set; } = "خطایی رخ داد !";
 }
